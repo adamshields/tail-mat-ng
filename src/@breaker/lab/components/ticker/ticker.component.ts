f61fff08c65cc0be2566f4424421460813c84aab
Adam Shields <adamenote@gmail.com>
2024-11-13 21:38:31 -0500

    added lab for ticker and all signal docs in angular

--- src/@breaker/lab/components/ticker/ticker.component.ts
+++ src/@breaker/lab/components/ticker/ticker.component.ts
@@ -1,11 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { interval } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 
 // Create a signal from an RxJs Observable with toSignal
 // https://next.angular.dev/ecosystem/rxjs-interop#create-a-signal-from-an-rxjs-observable-with-tosignal
+// use to theSignal function to create a signal which tracks the value of an observable
+// it behaves similar to the async pipe in templates but is more flexible and can be used anywhere in the application
+// Like the async pipe, toSignal subscribes to the Observable immediately, which may trigger side effects.
+// The subscription created by toSignal automatically unsubscribes from the given Observable when the component or service which calls toSignal is destroyed.
+
+
+
 
 @Component({
   selector: 'app-ticker',
@@ -17,4 +24,14 @@ export class Ticker {
   counterObservable = interval(1000);
   // Get a `Signal` representing the `counterObservable`'s value.
   counter = toSignal(this.counterObservable, {initialValue: 0});
+
+  constructor() {
+    const mySignal = signal(0);
+    const obs$ = toObservable(mySignal);
+    obs$.subscribe(value => console.log(value));
+    mySignal.set(1);
+    mySignal.set(2);
+    mySignal.set(3);
+  }
+
 }
